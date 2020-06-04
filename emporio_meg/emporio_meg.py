752e319d950f8f58620cc145480673ee1e85b772
Tabata Polli <tata_polli@hotmail.com>
2020-06-04 14:01:03 -0300

    Tarefa criacao do menu inicial concluida

--- emporio_meg/emporio_meg.py
+++ emporio_meg/emporio_meg.py
@@ -1,6 +1,25 @@
+from .menu import Menu 
+
 class EmporioMeg(object):
     def __init__(self):
         pass
 
     def run(self):
-        print("Hello Meg")
+        Menu.menu_principal()
+        opcao = int(input())
+        
+        while opcao != 0:
+            while opcao != 1 and opcao != 2:
+                Menu.opcao_invalida()
+                enter = str(input())
+            else:
+                if opcao == 1:
+                    Menu.login()
+                    Menu.menu_principal()
+                    opcao = int(input())
+                elif opcao == 2:
+                    Menu.ajuda()
+                    Menu.menu_principal()
+                    opcao = int(input())
+        else:
+            Menu.sair()
---
+++ emporio_meg/menu.py
@@ -0,0 +1,35 @@
+import os
+
+class Menu():
+    def menu_principal():
+        print("###########################")
+        print("        EMPORIO MEG        ")
+        print("###########################")
+        print("1 - LOGIN")
+        print("2 - AJUDA")
+        print("0 - SAIR")
+        print("SELECIONE UMA OPCAO: ")
+
+    def login():
+        os.system("cls")
+        print("#### LOGIN ####")
+        print("USUARIO: ")
+        usuario = str(input())
+        print("SENHA: ")
+        senha = str(input())
+    
+    def ajuda():
+        os.system("cls")
+        print("#### AJUDA ####")
+        print("VERSAO: 1.0.0")
+        print("CRIADO POR: TABATA POLLI")
+        print("PRESSIONE ENTER PARA CONTINUAR...")
+        enter = str(input())
+
+    def sair():
+        print("SAINDO...")
+        quit
+
+    def opcao_invalida():
+        print("OPCAO INDISPONIVEL.")
+        print("PRESSIONE ENTER PARA CONTINUAR...")
