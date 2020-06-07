e4b0d247360d5607e1d21659ac982c2f08cbde04
Tabata Polli <tata_polli@hotmail.com>
2020-06-07 15:04:27 -0300

    Tarefa novos menus concluida

--- emporio_meg/emporio_meg.py
+++ emporio_meg/emporio_meg.py
@@ -1,4 +1,5 @@
-from .menu import Menu 
+from .menu import Menu
+import os
 
 class EmporioMeg(object):
     
@@ -17,8 +18,46 @@ class EmporioMeg(object):
             else:
                 if opcao==1:
                     Menu.login()
-                    Menu.menu_principal()
+                    os.system("cls")
+                    Menu.submenu_login()
                     opcao = int(input())
+                    while opcao!=0 and opcao!=1 and opcao!=2 and opcao!=3 and opcao!=4 and opcao!=5: 
+                        Menu.opcao_invalida()
+                        enter = str(input())
+                    else:
+                        if opcao==1:
+                            os.system("cls")
+                            Menu.administrador()
+                            enter = str(input())
+                            Menu.submenu_login()
+                            opcao = int(input())
+                        if opcao==2:
+                            os.system("cls")
+                            Menu.submenu_seguranca()
+                            enter = str(input())
+                            Menu.submenu_login()
+                            opcao = int(input())
+                        if opcao==3:
+                            os.system("cls")
+                            Menu.submenu_estoque()
+                            enter = str(input())
+                            Menu.submenu_login()
+                            opcao = int(input())
+                        if opcao==4:
+                            os.system("cls")
+                            Menu.submenu_caixa()
+                            enter = str(input())
+                            Menu.submenu_login()
+                            opcao = int(input())
+                        if opcao==5:
+                            os.system("cls")
+                            Menu.submenu_financeiro()
+                            enter = str(input())
+                            Menu.submenu_login()
+                            opcao = int(input())
+                        if opcao==0:
+                            Menu.menu_principal()
+                            opcao = int(input())
                 elif opcao==2:
                     Menu.ajuda()
                     Menu.menu_principal()
--- emporio_meg/menu.py
+++ emporio_meg/menu.py
@@ -11,6 +11,7 @@ class Menu():
         print("1 - LOGIN")
         print("2 - AJUDA")
         print("0 - SAIR")
+        print(" ")
         print("SELECIONE UMA OPCAO: ")
 
     def login():
@@ -20,6 +21,63 @@ class Menu():
         usuario = str(input())
         senha = getpass.getpass("SENHA: ")
         
+    def submenu_login():
+        print("################################")
+        print("      SISTEMA EMPORIO MEG       ")
+        print("################################")
+        print("1 - ADMINISTRADOR")
+        print("2 - SEGURANÇA")
+        print("3 - ESTOQUE")
+        print("4 - CAIXA")
+        print("5 - FINANCEIRO")
+        print("0 - VOLTAR")
+        print(" ")
+        print("SELECIONE UMA OPCAO: ") 
+
+    def administrador():
+        print("#### ADMINISTRADOR ####")
+        print(" ")
+        print("PRESSIONE ENTER PARA CONTINUAR...")                      
+
+    def submenu_seguranca():
+        print("####   SEGURANCA   ####")
+        print(" ")
+        print("1 - CADASTRAR USUÁRIO")
+        print("2 - BUSCAR USUÁRIO")
+        print("3 - LISTAR USUÁRIOS")
+        print("0 - VOLTAR")
+        print(" ")
+        print("PRESSIONE ENTER PARA CONTINUAR: ") 
+
+    def submenu_estoque():
+        print("####   ESTOQUE   ####")
+        print(" ")
+        print("1 - CADASTRAR PRODUTO")
+        print("2 - BUSCAR PRODUTO")
+        print("3 - EXIBIR ESTOQUE")
+        print("0 - VOLTAR")
+        print(" ")
+        print("PRESSIONE ENTER PARA CONTINUAR: ") 
+
+    def submenu_caixa():
+        print("####   CAIXA   ####")
+        print(" ")
+        print("1 - ABRIR CAIXA")
+        print("2 - REGISTRAR COMPRA")
+        print("3 - FECHAR CAIXA")
+        print("0 - VOLTAR")
+        print(" ")
+        print("PRESSIONE ENTER PARA CONTINUAR: ") 
+
+    def submenu_financeiro():
+        print("####   FINANCEIRO   ####")
+        print(" ")
+        print("1 - RELATORIO DIARIO")
+        print("2 - RELATORIO MENSAL")
+        print("0 - VOLTAR")
+        print(" ")
+        print("PRESSIONE ENTER PARA CONTINUAR: ") 
+
     def ajuda():
         os.system("cls")
         print("#### AJUDA ####")
